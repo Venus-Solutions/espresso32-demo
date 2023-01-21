09380af33630ee0b1d1bb394e50188650713d369
Tom Dhanabhon <thanaphon.spk@gmail.com>
2023-01-21 20:37:22 +0700

    Update main.cpp

--- Espresso32/src/main.cpp
+++ Espresso32/src/main.cpp
@@ -6,13 +6,16 @@
 
 // Define I/O pins
 #define LED_PIN       16   // Built-in LED pin
+#define LED_EXT_PIN   17   // External LED pin
 
 // Helper macro to calculate array size
 #define COUNT_OF(x) ((sizeof(x)/sizeof(0[x])) / ((size_t)(!(sizeof(x) % sizeof(0[x])))))
 
 void initilizeWiFi(void);
 const bool reconnect(void);
+void ledBlink(void);
 RPC_Response processSetGpioState(const RPC_Data &data);
+RPC_Response processGetGpioState(const RPC_Data &data);
 
 constexpr char WIFI_SSID[] PROGMEM = "YOUR_WIFI_SSID"; 
 constexpr char WIFI_PASSWORD[] PROGMEM = "YOUR_WIFI_PASSWORD";
@@ -39,13 +42,19 @@ WiFiClient espClient;
 ThingsBoardSized<MAX_MESSAGE_SIZE> tb(espClient);
 
 // RPC handlers
-RPC_Callback callback = {"setValue", processSetGpioState};
+// RPC_Callback callback = {"setValue", processSetGpioState};
+
+RPC_Callback callbacks[] = {
+  { "setValue",         processSetGpioState },
+  { "getValue",         processGetGpioState },
+};
 
 // Set to true if application is subscribed for the RPC messages.
 bool subscribed = false;
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_EXT_PIN, OUTPUT);
   
   // Initalize serial connection for debugging
   Serial.begin(SERIAL_DEBUG_BAUD);
@@ -76,9 +85,20 @@ void loop() {
 
     // Perform a subscription. All consequent data processing will happen in
     // callbacks as denoted by callbacks[] array.
-    if (!tb.RPC_Subscribe(callback)) {
-      Serial.println("Failed to subscribe for RPC");
-      return;
+    // if (!tb.RPC_Subscribe(callback)) {
+    //   Serial.println("Failed to subscribe for RPC");
+    //   return;
+    // }
+    // if (!tb.RPC_Subscribe(callbacks, String(COUNT_OF(callbacks)).c_str())) {
+    //   Serial.println("Failed to subscribe for RPC");
+    //   return;
+    // }
+
+    for (size_t i = 0; i < COUNT_OF(callbacks); i++) {
+      if (!tb.RPC_Subscribe(callbacks[i])) {
+        Serial.println("Failed to subscribe for RPC");
+        return;
+      }
     }
 
     Serial.println("Subscribe done");
@@ -86,6 +106,8 @@ void loop() {
   }
 
   tb.loop();
+
+  ledBlink();
 }
 
 void initilizeWiFi(void) {
@@ -98,6 +120,15 @@ void initilizeWiFi(void) {
     Serial.print(".");
   }
   Serial.println("Connected to AP");
+}
+
+void ledBlink(void) {
+  digitalWrite(LED_EXT_PIN, LOW);
+  tb.sendAttributeBool("ext_value", digitalRead(LED_EXT_PIN));
+  delay(500);
+  digitalWrite(LED_EXT_PIN, HIGH);
+  tb.sendAttributeBool("ext_value", digitalRead(LED_EXT_PIN));
+  delay(500);
 }
 
 const bool reconnect(void) {
@@ -122,6 +153,17 @@ RPC_Response processSetGpioState(const RPC_Data &data) {
 
   digitalWrite(LED_PIN, !state ? HIGH : LOW);
   tb.sendAttributeBool("value", !digitalRead(LED_PIN));
+
+  return RPC_Response(NULL, data);
+}
+
+RPC_Response processGetGpioState(const RPC_Data &data) {
+  Serial.println("Received the get GPIO RPC method");
+
+  int state = data;
+
+  Serial.print("Get GPIO state ");
+  Serial.println(state);
 
   return RPC_Response(NULL, data);
 }
